52719b13f02d945382f4433c5c81014a0c203670
Dario Caberlotto <dario.caber@gmail.com>
2025-11-19 18:08:57 +0100

    add logging

--- generative_ai/factories.py
+++ generative_ai/factories.py
@@ -1,17 +1,23 @@
 from abstracts import GenerativeAIClient
-
 from providers import Provider
-from clients import GoogleGenAIClient, OpenAIClient, OpenAIClientAsync
+
+import logging
 
 # Factory to create GenerativeAIClient instances based on provider
 class GenerativeAIClientFactory:
     @staticmethod
     def create_client(provider: Provider, api_key: str) -> GenerativeAIClient:
         if provider == Provider.GOOGLE:
+            from clients import GoogleGenAIClient
+            logging.info("Creationg GoogleGenAIClient")
             return GoogleGenAIClient(api_key=api_key)
         elif provider == Provider.OPENAI:
+            from clients import OpenAIClient
+            logging.info("Creationg OpenAIClient")
             return OpenAIClient(api_key=api_key)
         elif provider == Provider.OPENAI_ASYNC:
+            from clients import OpenAIClientAsync
+            logging.info("Creationg OpenAIClientAsync")
             return OpenAIClientAsync(api_key=api_key)
         else:
             raise ValueError(f"Unknown provider: {provider}")
