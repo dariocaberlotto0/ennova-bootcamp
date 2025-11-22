34794929c14b9c0421018bf90fa3a988f8e314c1
Dario Caberlotto <dario.caber@gmail.com>
2025-11-22 15:52:39 +0100

    implementation of llms

--- src/student_prompt_logger/app/services/client_factory.py
+++ src/student_prompt_logger/app/services/client_factory.py
@@ -1,25 +1,7 @@
 from .llm import LLMClient
-import os
+from main import GOOGLE_KEY, OPENAI_KEY
 
 import logging
-
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except Exception as e:
-    logging.error(e)
-
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-if not GOOGLE_API_KEY:
-    logging.warning("⚠️ Set GOOGLE_API_KEY in your environment to run live calls.")
-else:
-    logging.info("✅ GOOGLE_API_KEY is set.")
-
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-if not OPENAI_API_KEY:
-    logging.warning("⚠️ Set GOOGLE_API_KEY in your environment to run live calls.")
-else:
-    logging.info("✅ OPENAI_API_KEY is set.")
 
 # Factory to create llm instances based on provider
 class ClientFactory:
@@ -29,15 +11,15 @@ class ClientFactory:
         if provider == "gemini":
             from .llm import GeminiLLM
             logging.info("Creating GeminiLLM")
-            return GeminiLLM(api_key=GOOGLE_API_KEY)
+            return GeminiLLM(api_key=GOOGLE_KEY)
         elif provider == "openai":
             from .llm import OpenAILLM
             logging.info("Creating OpenAILLM")
-            return OpenAILLM(api_key=OPENAI_API_KEY)
+            return OpenAILLM(api_key=OPENAI_KEY)
         elif provider == "openaiasync":
             from .llm import OpenAILLMAsync
             logging.info("Creating OpenAILLMAsync")
-            return OpenAILLMAsync(api_key=OPENAI_API_KEY)
+            return OpenAILLMAsync(api_key=OPENAI_KEY)
         elif provider == "mock":
             from .llm import MockLLM
             logging.info("Creating MockLLM")
